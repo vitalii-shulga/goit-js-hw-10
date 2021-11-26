6409aaa6ff536fd6636d1f5afcb1577fcb3fa685
Vitalii Shulga <v.shulga019@gmail.com>
2021-11-26 21:47:43 +0200

    Updated name mapping

--- src/index.js
+++ src/index.js
@@ -38,8 +38,8 @@ function renderCountryList(countries) {
     .map(({ name, flags }) => {
       return `
           <li class="country-list__item">
-              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
-              <h2 class="country-list__name">${name.official}</h2>
+              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.common}" width = 30px height = 30px>
+              <h2 class="country-list__name">${name.common}</h2>
           </li>
           `
     })
