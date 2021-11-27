257c751457727a03cbda6a70d609b13e1be1b4d3
Vitalii Shulga <v.shulga019@gmail.com>
2021-11-27 11:21:34 +0200

    Updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css'
 import debounce from 'lodash.debounce'
 import Notiflix from 'notiflix'
-import { fetchCountries } from './js/fetchCountries'
+import { fetchCountries } from './js/fetch-countries'
 
 const DEBOUNCE_DELAY = 300
 
